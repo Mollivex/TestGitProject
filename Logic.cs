b3845ab26a2350dbe70539131fff0af9f47b96f0
Alex <licoms1997@gmail.com>
2023-08-01 22:55:28 +0300

    Add Logic class with ShowHelloWorld method

---
+++ Logic.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHubTestApp
+{
+    class Logic
+    {
+        public void ShowHelloWorld()
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
