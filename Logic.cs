2b9d6a9a0976437002b7fd89a1a04f058dc26c36
Alex <licoms1997@gmail.com>
2023-08-01 22:06:32 +0300

    Add Logic class and overloading method 'sqrtElem'

---
+++ Logic.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHubTestApp
+{
+    public class Logic
+    {
+        /// <summary>
+        /// Square number of element 'a'
+        /// </summary>
+        /// <param name="a"></param>
+        public void sqrtElem(int a)
+        {
+            int result = a * a;
+
+            Console.WriteLine(result);
+        }
+
+        /// <summary>
+        /// Square number of element 'a' raised to the power 'b'
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="p"></param>
+        /// <returns></returns>
+        public int sqrtElem(int a, int p)
+        {
+            return (int)Math.Pow(a, p);
+        }
+    }
+}
